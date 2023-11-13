4b4428c84877fd6e7904ece257c203289f519bfe
ggenna88 <gonzalogenna@gmail.com>
2023-11-13 19:23:36 -0300

    FormSelectedOption

--- src/Components/FormSelectOption.js
+++ src/Components/FormSelectOption.js
@@ -3,7 +3,7 @@ import Form from "react-bootstrap/Form";
 function OptionSelectDropdown({ listOption }) {
   return (
     <Form.Select aria-label="Default select example" className="my-3 w-75">
-      <option>Seleccionar...</option>
+      <option>Tipo de persona...</option>
       {listOption.map((option) => (
         <option value="1">{option}</option>
       ))}
