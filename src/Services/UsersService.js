d45641b3f4ddf4a163279e2b623927d8416966ec
ggenna88 <gonzalogenna@gmail.com>
2023-11-16 20:04:36 -0300

    userService

--- src/Services/UsersService.js
+++ src/Services/UsersService.js
@@ -43,4 +43,30 @@ async function AddUser(user) {
   }
 }
 
-export { GetAllUsers, AddUser };
+async function UpdateUser(user) {
+  const token = localStorage.getItem("token");
+
+  try {
+    var bearer = "Bearer " + token;
+    const response = await fetch(
+      "http://localhost:8080/users/update/{username}",
+      {
+        method: "PUT",
+        withCredentials: true,
+        body: JSON.stringify(user),
+        headers: {
+          Authorization: bearer,
+          "Content-Type": "application/json",
+        },
+        origin: "http://localhost:3000",
+        credentials: "include",
+        referrerPolicy: "strict-origin-when-cross-origin",
+      }
+    ).then((res) => res.ok);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export { GetAllUsers, AddUser, UpdateUser };
