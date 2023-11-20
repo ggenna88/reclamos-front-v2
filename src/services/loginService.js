c1b2829337284a02cdfff3f6f8f62734de0628fe
GCamin <gian-1998@hotmail.com>
2023-11-20 17:55:35 -0300

    commit inicial merge-final

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,23 +1,22 @@
-
 export async function LoginService(username, password) {
 
-  var URL = "http://localhost:8080/auth/login";
-  var data = { username, password };
+    var URL = "http://localhost:8080/auth/login";
+    var data = { username, password };
+    
+    const response = fetch(URL,{
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-Type": "application/json"}})
+      .then((r) => r.json())
+      .then((res) => res.token)
+      .catch((error) => console.log("Error: ", error));
+    return response;
+  };
   
-  const response = fetch(URL,{
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {"Content-Type": "application/json"}})
-    .then((r) => r.json())
-    .then((res) => res.token)
-    .catch((error) => console.log("Error: ", error));
-  return response;
-};
-
-export function DecodeJwt(jwt){
-
-  const token = jwt;
-  const [, payload] = token.split('.');
-  const decodedPayload = JSON.parse(atob(payload));
-  return decodedPayload.role;
-}
+  export function DecodeJwt(jwt){
+  
+    const token = jwt;
+    const [, payload] = token.split('.');
+    const decodedPayload = JSON.parse(atob(payload));
+    return decodedPayload.role;
+  }
